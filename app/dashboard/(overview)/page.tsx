e12486c647a6dcc7a003973f9568eae11dd35dc7
DevelopWithAP <apostolisparga@gmail.com>
2024-05-04 16:07:15 +0300

    Add search and pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,13 +9,6 @@ import { lusitana } from "../../ui/fonts"
 
 
 export default async function Page() {
-  // const {
-  //   numberOfCustomers, 
-  //   numberOfInvoices,
-  //   totalPendingInvoices,
-  //   totalPaidInvoices,
-  // } = await fetchCardData()
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
